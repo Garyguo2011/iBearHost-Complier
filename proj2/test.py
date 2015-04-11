555c0f5ad10caf73c3c4f898c6a787a05e078930
Xu_He <cs164-ee@s349-8.EECS.Berkeley.EDU>
2015-04-10 21:06:51 -0700

    tupleassign unresolved

--- proj2/test.py
+++ proj2/test.py
@@ -1,3 +1,9 @@
-a::int = 1
-b::str = a
-print b
+x :: tuple0 = ()
+y :: tuple1 of [int] = (4)
+z :: tuple2 of [int, str] = (4, "hello")
+m :: tuple3 of [int, str, bool] = (4, "hello", True)
+
+print x
+print y
+print z
+print m
