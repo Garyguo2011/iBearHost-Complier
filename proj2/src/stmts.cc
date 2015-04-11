b119eded5f5e7b896b7d462bffcea5a5227591fd
Jinge Zhang <jingezhang@berkeley.edu>
2015-04-11 13:50:06 -0700

    add documentation to every single method that we implemented

--- proj2/src/stmts.cc
+++ proj2/src/stmts.cc
@@ -29,7 +29,7 @@ protected:
 
 NODE_FACTORY (Println_AST, PRINTLN);
 
-/** Print */
+/** Print AST Tree for print, inherited from Println_AST. */
 class Print_AST : public Println_AST {
 protected:
     NODE_CONSTRUCTORS(Print_AST, Println_AST);
@@ -56,7 +56,7 @@ protected:
 
 NODE_FACTORY (StmtList_AST, STMT_LIST);
 
-/** return */
+/** Return AST Tree for return, inherited from AST_Tree. */
 class Return_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Return_AST, AST_Tree);
@@ -100,7 +100,7 @@ protected:
 
 NODE_FACTORY(Return_AST, RETURN);
 
-/** Break */
+/** Break AST Tree for break, inherited from AST_Tree. */
 class Break_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Break_AST, AST_Tree);
@@ -108,7 +108,7 @@ protected:
 
 NODE_FACTORY(Break_AST, BREAK);
 
-/** Continue */
+/** Continue AST Tree for continue, inherited from AST_Tree. */
 class Continue_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Continue_AST, AST_Tree);
@@ -116,11 +116,12 @@ protected:
 
 NODE_FACTORY(Continue_AST, CONTINUE);
 
-/** If*/
+/** If AST Tree for if, inherited from AST_Tree. */
 class If_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(If_AST, AST_Tree);
 
+    /** Throw an erro if the first child's type(condition) is not a boolDecl. */
     void resolveTypes (Decl* context, Unifier& subst) {
         AST::resolveTypes (context, subst);
         if (!unify(child(0)->getType(), boolDecl->asType(), subst)) {
@@ -132,11 +133,12 @@ protected:
 
 NODE_FACTORY(If_AST, IF);
 
-/** While */
+/** While AST Tree for while, inherited from AST_Tree. */
 class While_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(While_AST, AST_Tree);
 
+    /** Throw an erro if the first child's type(condition) is not a boolDecl. */
     void resolveTypes (Decl* context, Unifier& subst) {
         AST::resolveTypes(context, subst);
 
@@ -149,11 +151,13 @@ protected:
 
 NODE_FACTORY(While_AST, WHILE);
 
-/** For */
+/** For AST Tree for for, inherited from AST_Tree. */
 class For_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(For_AST, AST_Tree);
 
+    /** First child would a target. Then for every child, recursively call
+     *  collectDecls to ENCLOSING. Finally addTargetDecls from target to ENCLOSING.*/
     void collectDecls (Decl* enclosing) {
         AST_Ptr target = child(0);
         for (size_t i = 1; i < arity (); i += 1) {
@@ -162,6 +166,11 @@ protected:
         target->addTargetDecls (enclosing);
     }
 
+    /** For T in E. Typechecking on E's type $a and T's type $b.
+     *  Do nothing if $a is one of list($b), range or str
+     *  and $a is list($b)
+     *  or $a is range and $b is int
+     *  or $a and $b are str. Otherwise throw an error. */
     void resolveTypes (Decl* context, Unifier& subst) {
         AST::resolveTypes(context, subst);
 
@@ -184,11 +193,13 @@ protected:
 
 NODE_FACTORY(For_AST, FOR);
 
-/** def */
+/** Def AST Tree for def, inherited from AST_Tree.  */
 class Def_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Def_AST, AST_Tree);
 
+    /** Recursively can resolveSimpleIds within the scope of first child'decl environment,
+     *  for every child variable. */
     AST_Ptr resolveSimpleIds (const Environ* env)
     {
         AST_Ptr id = child(0);
@@ -199,6 +210,9 @@ protected:
         return this;
     }
 
+    /** Add declaration to Id if it can be added DefDecl. Then recursively
+     *  can it for every child. Throw an error if the child's name equal to
+     *  the Id's name.  */
     void collectDecls (Decl* enclosing)
     {
         AST_Ptr id = child(0);
@@ -218,11 +232,15 @@ protected:
 
 NODE_FACTORY(Def_AST, DEF);
 
-/** Method */
+/** AST Tree for Method definition, inherited from the Def_AST */
 class Method_AST : public Def_AST {
 protected:
     NODE_CONSTRUCTORS(Method_AST, Def_AST);
 
+    /** First check whether the first parameter is "self". If not, throw an error.
+     *  Otherwise, add declaration to Id if it can be added DefDecl. Then recursively
+     *  can it for every child. Throw an error if the child's name equal to
+     *  the Id's name.  */
     void collectDecls (Decl* enclosing)
     {
         if ((std::string) child(1)->child(0)->child(0)->as_string().c_str() == (std::string) "self") {
@@ -246,11 +264,13 @@ protected:
 
 NODE_FACTORY(Method_AST, METHOD);
 
-/** Formalslist*/
+/** Formalslist AST for Formals in grammar rule, inherited from AST_Tree. */
 class Formalslist_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Formalslist_AST, AST_Tree);
 
+    /** Add declaration to every child if it can be add Paramdecl, divided into
+     *  two seperate scenarios, either child is a TYPED_ID or otherwise. */
     void collectDecls (Decl* enclosing)
     {
         int count = 0;
@@ -279,12 +299,14 @@ protected:
 
 NODE_FACTORY(Formalslist_AST, FORMALS_LIST);
 
-/** Class */
+/** Class AST Tree for class, inherited from AST_Tree. */
 class Class_AST : public AST_Tree {
 protected:
 
     NODE_CONSTRUCTORS(Class_AST, AST_Tree);
 
+    /** Recursively can resolveSimpleIds within the scope of first child'decl environment,
+     *  for every child variable. */
     AST_Ptr resolveSimpleIds (const Environ* env)
     {
         AST_Ptr id = child(0);
@@ -295,6 +317,8 @@ protected:
         return this;
     }
 
+    /** Add declarations that result from occurrences of type variables
+     *  in type attributions to each child variable of the second child. */
     void collectTypeVarDecls (Decl* enclosing){
         AST_Ptr params = child(1);
         for_each_child_var(param, params) {
@@ -304,6 +328,9 @@ protected:
         } end_for;
     }
 
+    /** If the class name comes from the prelude, then construct class declaration to
+     *  specific decl that declared in decls.cc, otherwise add decl to Id if the decl
+     *  is not NULL and Id's name hasn't been declared in the scope of ENCLOSING. */
     void collectDecls (Decl* enclosing) {
         
         AST_Ptr id = child(0);
@@ -409,7 +436,7 @@ protected:
 
 NODE_FACTORY(Class_AST, CLASS);
 
-/** Block */
+/** Block AST Tree for break, inherited from AST_Tree. */
 class Block_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Block_AST, AST_Tree);
@@ -417,7 +444,7 @@ protected:
 
 NODE_FACTORY(Block_AST, BLOCK);
 
-/** Native */
+/** Native AST Tree for native, inherited from AST_Tree. */
 class Native_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(Native_AST, AST_Tree);
@@ -425,7 +452,7 @@ protected:
 
 NODE_FACTORY(Native_AST, NATIVE);
 
-/** TypeFormalsList*/
+/** TypeFormalsList AST Tree for TypeFormals from grammar rule, inherited from AST_Tree.*/
 class TypeFormalsList_AST : public AST_Tree {
 protected:
     NODE_CONSTRUCTORS(TypeFormalsList_AST, AST_Tree);
@@ -450,7 +477,7 @@ protected:
 
 NODE_FACTORY(TypeFormalsList_AST, TYPE_FORMALS_LIST);
 
-/** TypedID */
+/** TypedID AST Tree for TypedId from the grammar rule, inherited from Typed_Tree. */
 class TypedID_AST : public Typed_Tree {
     
 protected:
@@ -461,6 +488,8 @@ protected:
         return child(0);
     }
 
+    /** If the Id could be add as Variable Declaration, then set my type to this delcation
+     *  and add it to the ID. */
     void addTargetDecls (Decl* enclosing) {
         AST_Ptr id = getId();
         Decl* decl = enclosing->addVarDecl(id);
@@ -470,11 +499,13 @@ protected:
         }
     }
 
+    /** Return the second child as a Type_Ptr. */
     Type_Ptr getType()
     {
         return (Type_Ptr) child(1);
     }
 
+    /** Set type to my type iff it is a TYPE node with an arity of 2. */
     void resolveTypes (Decl* context, Unifier& subst)
     {
         if (arity() == 2 && child(1)->oper()->syntax() == TYPE){
@@ -486,17 +517,21 @@ protected:
 
 NODE_FACTORY(TypedID_AST, TYPED_ID);
 
-/** Assign */
+/** Assign AST Tree for assign, inherited from Typed_Tree. */
 class Assign_AST : public Typed_Tree {
 protected:
     NODE_CONSTRUCTORS(Assign_AST, Typed_Tree);
 
+    /** Add target declarations from first child to ENCLOSING.
+     *  Add declarations from the second child to ENCLOSING. */
     void collectDecls (Decl* enclosing)
     {
         child(0)->addTargetDecls(enclosing);
         child(1)->collectDecls(enclosing);
     }
 
+    /** Typechecking on first child and second child. Set the type to it
+     *  if types for these two children can be unified, otherwise throw an error. */
     void resolveTypes (Decl* context, Unifier& subst)
     {
         AST::resolveTypes (context, subst);
