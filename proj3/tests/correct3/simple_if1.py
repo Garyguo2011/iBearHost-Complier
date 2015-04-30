072ddd6f1f9eca7a432a3d62779829709da6a2f7
Xu_He <cs164-ee@s349-4.EECS.Berkeley.EDU>
2015-04-29 17:35:36 -0700

    before merge with dev-jz

---
+++ proj3/tests/correct3/simple_if1.py
@@ -0,0 +1,19 @@
+if True:
+	print 1
+else:
+	print -1
+
+if False:
+	print 1
+else:
+	print -1
+
+# x = 1
+# if x == 0:
+# 	print 'no'
+# elif x == 2:
+# 	print 'no'
+# elif x == 1:
+# 	print 'yes'
+# else:
+# 	print 'no'
