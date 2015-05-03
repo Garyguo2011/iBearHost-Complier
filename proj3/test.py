c91edc4ef4aade7a504396676c53bd4b25029a95
Leyuan_Jia <cs164-cg@torus.CS.Berkeley.EDU>
2015-05-02 23:14:50 -0700

    fail 3 tests

--- proj3/test.py
+++ proj3/test.py
@@ -1,8 +1,18 @@
-a = [1, 2, 3]
-c :: list of [int] = [1,2,3,4,5]
-for a[0] in c:
-	print a
+class C:
+    def __init__(self):
+        pass
 
-# d :: list of [str] = ["a", "b", "c"]
-# for el in d:
-# 	print el
+    def g (self):
+        return 6
+
+vg::(C)->int = C.g
+
+anC::C = C()
+
+print vg(anC)
+
+List = [C.g]
+
+vg = List[0]
+
+print vg(anC)
